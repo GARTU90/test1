2454f1bec4cff8cd438bf9e3cd407780fcb2ae63
GARTU90 <vgarredo@outlook.com>
2023-09-25 13:23:42 -0600

    try except implement

--- example1.py
+++ example1.py
@@ -2,6 +2,13 @@
 #by juan.daniel.rangel.avila@gmail.com
 #GNU/GPL License
 import numpy as np
-x=np.pi
-x=np.cos(x)
-print(x)
+data_list=np.arange(1,11,1)
+for x in data_list:
+    print ("value: "+str(x))
+x="hola"
+try:
+    z=int(x)+1
+except:
+    print("todo tonto no jalo xd")
+    #x=np.cos(x)
+print(data_list)
